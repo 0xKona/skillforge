424f56892fb5c7b65456f857186da155dbfd2d10
0xKona <konarobinson@outlook.com>
2025-10-15 13:21:43 +0100

    update-auth-config

--- src/configs/amplify.config.ts
+++ src/configs/amplify.config.ts
@@ -1,10 +1,16 @@
 const amplifyConfig = {
-  aws_project_region: process.env.NEXT_PUBLIC_AWS_PROJECT_REGION,
-  aws_cognito_region: process.env.NEXT_PUBLIC_AWS_CONGNITO_REGION,
+  aws_project_region: process.env.NEXT_PUBLIC_AWS_REGION,
+  aws_cognito_region: process.env.NEXT_PUBLIC_AWS_REGION,
   aws_user_pools_id: process.env.NEXT_PUBLIC_AWS_USER_POOLS_ID,
   aws_user_pools_web_client_id:
     process.env.NEXT_PUBLIC_AWS_USER_POOL_WEB_CLIENT_ID,
-  oauth: {},
+  oauth: {
+    domain: process.env.NEXT_PUBLIC_COGNITO_DOMAIN!,
+    scopes: ['openid', 'email', 'profile', 'aws.cognito.signin.user.admin'],
+    redirectSignIn: [process.env.NEXT_PUBLIC_REDIRECT_SIGN_IN || 'http://localhost:3000/'],
+    redirectSignOut: [process.env.NEXT_PUBLIC_REDIRECT_SIGN_OUT || 'http://localhost:3000/'],
+    responseType: 'code',
+  },
 };
 
 export { amplifyConfig };
