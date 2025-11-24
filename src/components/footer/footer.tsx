47de14569164bfb20d5e9368708c3aa453685dec
Connor Robinson <connor.3.robinson@bt.com>
2025-11-24 13:16:44 +0000

    scaffold new profile management page

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -3,7 +3,7 @@ import { TypographyP } from '../ui/typography/typography';
 
 export default function Footer() {
     return (
-        <footer className="flex gap-6 p-6 flex-wrap items-center justify-center bg-slate-950 w-full text-slate-50">
+        <footer className="flex gap-6 p-6 flex-wrap items-center justify-center mt-auto bg-slate-950 w-full text-slate-50">
             <TypographyP>Temporary footer</TypographyP>
             <ThemeToggle />
         </footer>
