1625a2067fe489ea5b879845f6d3652cbaeb7d0b
0xKona <konarobinson@outlook.com>
2025-11-27 13:10:34 +0000

    fix conflicts

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -1,21 +1,13 @@
 import { defineBackend } from '@aws-amplify/backend';
 import { auth } from './auth/resource';
-<<<<<<< HEAD
 // import { data } from './data/resource';
-// import { storage } from './storage/resource';
-=======
-import { data } from './data/resource';
->>>>>>> 858d52f (Initialize amplify backend (#4))
+import { storage } from './storage/resource';
 
 /**
  * @see https://docs.amplify.aws/react/build-a-backend/ to add storage, functions, and more
  */
 defineBackend({
     auth,
-<<<<<<< HEAD
     // data,
-    // storage,
-=======
-    data,
->>>>>>> 858d52f (Initialize amplify backend (#4))
+    storage,
 });
